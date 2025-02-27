11dc7cc8241614851054c19adc5f505ca800a080
Bhanu prakash <sigebhanuprakash171@gmail.com>
2025-02-27 10:54:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("bookSlot", (doctorId) => {
+    console.log(`User ${socket.id} booked a slot.`);
+    
+    // Notify all users **except the one who booked**
+    socket.broadcast.emit("slotAlert", doctorId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
