91a255a0e5c6f48bf3bd3b12c7371a97c83efac2
Bhanu prakash <sigebhanuprakash171@gmail.com>
2025-02-27 11:08:55 +0530

    fontend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = app.listen(3000, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://appointmentbookingweb.vercel.app/",
   },
 });
 
